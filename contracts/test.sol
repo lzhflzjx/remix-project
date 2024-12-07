d1e66cda803df864d36b69d077708534842489b7
lzhflzjx <1601492347@qq.com>
2024-12-08 00:13:48 +0800

    feat:基本数据结构

--- contracts/test.sol
+++ contracts/test.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+// struct
+// array
+// mapping
 contract HelloWorld {
     string strVal = "Hello World";
 
@@ -10,14 +13,21 @@ contract HelloWorld {
         address addr;
     }
 
-    function sayHello() public view returns (string memory) {
-        return addinfo(strVal);
+    Info[] infos;
+
+    mapping(uint256 => Info) infoMapping;
+
+    function sayHello(uint256 _id) public view returns (string memory) {
+        if (infoMapping[_id].addr == address(0x0)) {
+            return addinfo(strVal);
+        }
+        
+        return addinfo(infoMapping[_id].phrace);
     }
 
     function setHelloWorld(string memory newString, uint256 _id) public {
-        strVal = newString;
-
         Info memory info = Info(newString, _id, msg.sender);
+        infoMapping[_id] = info;
     }
 
     function addinfo(string memory helloWorldStr)
@@ -25,6 +35,6 @@ contract HelloWorld {
         pure
         returns (string memory)
     {
-        return string.concat(helloWorldStr, "from iron contract");
+        return string.concat(helloWorldStr, "from Spark contract");
     }
 }
