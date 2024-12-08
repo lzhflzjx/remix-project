61606c05e0aabd9fd75f53942a26f9e9e4bd7089
lzhflzjx <1601492347@qq.com>
2024-12-08 21:52:45 +0800

    feat:fundme函数

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,23 +3,25 @@ pragma solidity ^0.8.7;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-// 3.在锁定期内达到目标值，生产可以提款
-// 4.在锁定期内没有达到目标值，投资人可以退款
+
+
 
 contract FundMe {
     mapping(address => uint256) public funderToAmount;
 
-    uint256 MINMUM_VALUE = 100 * 10**18; //限定收款最小值 wei
+    uint256 MINMUM_VALUE = 10 * 10**18; //限定收款最小值 wei
 
-    AggregatorV3Interface internal dataFeed; 
+    AggregatorV3Interface internal dataFeed;
 
-    uint256 constant TARGET = 1000 * 10 ** 18;
+    uint256 constant TARGET = 100 * 10**18;
 
-    address owner;
+    address public owner;
 
     constructor() {
         // sepolia-testnet
-        dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        dataFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
         owner = msg.sender;
     }
 
@@ -46,20 +48,42 @@ contract FundMe {
         return answer;
     }
 
-    function convertEthToUsd(uint256 ethAmount) internal view  returns(uint256){
+    function convertEthToUsd(uint256 ethAmount)
+        internal
+        view
+        returns (uint256)
+    {
         // (ETH amount) * (ETH price) = (ETH value)
         uint256 ethPrice = uint256(getChainlinkDataFeedLatestAnswer());
         // ethAmount 单位是wei； ethPrice的精确度 precision 10**8
-        return ethAmount * ethPrice / (10 ** 8);
-    }
-
-    function getFund() external view {
-         require(convertEthToUsd(address(this).balance) >= TARGET,"target is not reached");
+        return (ethAmount * ethPrice) / (10**8);
     }
 
     function transferOwnerShip(address newOwner) public {
-        require(msg.sender == owner,"this function can only be called by owner");
+        require(
+            msg.sender == owner,
+            "this function can only be called by owner"
+        );
         owner = newOwner;
-        require(convertEthToUsd(address(this).balance) >= TARGET,"target is not reached");
+        require(
+            convertEthToUsd(address(this).balance) >= TARGET,
+            "target is not reached"
+        );
+    }
+    // 3.在锁定期内达到目标值，生产可以提款
+    // 4.在锁定期内没有达到目标值，投资人可以退款
+    function getFund() external {
+        require(
+            convertEthToUsd(address(this).balance) >= TARGET,
+            "target is not reached"
+        );
+        require(
+            msg.sender == owner,
+            "this function can only be called by owner"
+        );
+        //  transfer 纯转账 transfer ETH  and revert if tx failed
+        payable(msg.sender).transfer(address(this).balance);
+        // send
+        // call
     }
 }
